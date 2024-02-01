802c5b12672c17572d9d76da674b8b3375ebb140
Basangouda Patil <basangouda@Basangoudas-MacBook-Pro.local>
2024-01-31 21:14:18 -0800

    learnt static libraries

---
+++ _app/src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../header/add.h"
+#include "../header/multiply.h"
+
+int main(){
+
+    int a =5;
+
+    // printf("hello world\n");
+
+    int c = add(5, 5);
+
+    // printf("%d\n", c);
+
+    int d = multiply(5, 5);
+
+    // printf("%d\n", d);
+
+    return 0;
+}
